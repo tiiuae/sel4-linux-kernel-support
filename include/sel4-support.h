bdae72ad3970e342d3f5ec57713754a5b2125792
Ivan Kuznetsov <jsvapiav@gmail.com>
2023-01-14 12:33:15 +0200

    Add sel4-support header files
    jira: HYPR-432
    
    Signed-off-by: Ivan Kuznetsov <jsvapiav@gmail.com>

---
+++ include/sel4-support.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright 2020, Data61, CSIRO (ABN 41 687 119 230)
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+#ifndef _SEL4_SUPPORT_H
+#define _SEL4_SUPPORT_H
+
+#include <sel4/errors.h>
+#include <sel4/syscalls.h>
+#include <sel4/syscalls_names.h>
+#include <sel4/syscalls_funcs.h>
+#include <sel4/benchmark_track_types.h>
+
+
+#endif /* _SEL4_SUPPORT_H */
